21314d681a48424382155b63bb89580832350aae
chris <chris@xtralife.cloud>
2018-03-06 01:02:18 +0000

    it's getting better

--- CertPump.go
+++ CertPump.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"encoding/json"
+	"fmt"
 	"log"
 	"os"
 	"runtime"
 	"time"
 
-	"github.com/chrisDeFouRire/Heartbleed/heartbleed"
 	nats "github.com/nats-io/go-nats"
 )
+
+type cert struct {
+	Issuer             string   `json:"issuer"`
+	DNSnames           []string `json:"altNames"`
+	SignatureAlgorithm string   `json:"signatureAlgorithm"`
+	NotBefore          string   `json:"notBefore"`
+	NotAfter           string   `json:"notAfter"`
+	SerialNumber       string   `json:"serialNumber"`
+	Chain              []string `json:"chain"`
+}
 
 type request struct {
 	Hostname string `json:"hostname"`
@@ -18,20 +30,85 @@ type request struct {
 }
 
 type response struct {
-	Hostname   string `json:"hostname"`
-	Host       string `json:"host"`
-	Port       int32  `json:"port"`
-	Vulnerable bool   `json:"vulnerable"`
-	Error      string `json:"error,omitifempty"`
+	Hostname string `json:"hostname"`
+	Host     string `json:"host"`
+	Port     int32  `json:"port"`
+	Cert     cert   `json:"cert"`
+	Error    string `json:"error,omitempty"`
+}
+
+func getCert(hostname string, ip string, port int32) (*cert, error) {
+
+	address := fmt.Sprintf("%s:%d", ip, port)
+	conf := &tls.Config{
+		MinVersion:         tls.VersionSSL30,
+		MaxVersion:         tls.VersionTLS12,
+		InsecureSkipVerify: true,
+		ServerName:         hostname,
+	}
+
+	conn, err := tls.Dial("tcp", address, conf)
+	if err != nil {
+		return nil, err
+	}
+
+	tlsState := conn.ConnectionState()
+	if len(tlsState.PeerCertificates) == 0 {
+		return nil, fmt.Errorf("no certificates found")
+	}
+	_c := tlsState.PeerCertificates[0]
+
+	names := _c.DNSNames
+	if len(names) == 0 {
+		names = append(names, _c.Subject.CommonName)
+	}
+
+	c := &cert{
+		DNSnames:           names,
+		Issuer:             _c.Issuer.CommonName,
+		NotBefore:          _c.NotBefore.Format(time.RFC1123Z),
+		NotAfter:           _c.NotAfter.Format(time.RFC1123Z),
+		SerialNumber:       _c.SerialNumber.Text(16),
+		SignatureAlgorithm: _c.SignatureAlgorithm.String(),
+		Chain:              nil,
+	}
+
+	opts := x509.VerifyOptions{
+		Roots:         conf.RootCAs,
+		CurrentTime:   time.Now(),
+		DNSName:       conf.ServerName,
+		Intermediates: x509.NewCertPool(),
+	}
+	for i, cert := range tlsState.PeerCertificates {
+		c.Chain = append(c.Chain, cert.Subject.CommonName)
+		if i == 0 {
+			continue
+		}
+		opts.Intermediates.AddCert(cert)
+	}
+
+	_, certerr := tlsState.PeerCertificates[0].Verify(opts)
+
+	if certerr != nil && len(tlsState.PeerCertificates) == 1 {
+		if tlsState.PeerCertificates[0].Issuer.CommonName == tlsState.PeerCertificates[0].Subject.CommonName {
+			certerr = fmt.Errorf("x509: self signed cert")
+		} else {
+			certerr = fmt.Errorf("x509: incomplete cert chain")
+		}
+	}
+
+	conn.Close()
+	return c, certerr
 }
 
 func handleRequest(nc *nats.Conn, msg []byte, reply string) {
 	// don't let a panic crash the app or we'll crash all concurrent requests
+	/* Use DEV env var
 	defer func() {
 		if r := recover(); r != nil {
-			log.Println("Error recovered")
+			log.Println("Error recovered: ", r)
 		}
-	}()
+	}() */
 
 	req := request{}
 	jierr := json.Unmarshal(msg, &req)
@@ -39,39 +116,26 @@ func handleRequest(nc *nats.Conn, msg []byte, reply string) {
 		log.Println("JSON request unmarshal Error: ", jierr.Error())
 	}
 
-	tgt := &heartbleed.Target{
-		Service:  "https",
-		HostIp:   req.Host,
-		Hostname: req.Hostname,
-		Port:     req.Port,
-	}
-
-	_, err := heartbleed.Heartbleed(tgt,
-		[]byte("github.com/FiloSottile/Heartbleed"), true)
-
 	res := response{
 		Hostname: req.Hostname,
 		Host:     req.Host,
 		Port:     req.Port,
 	}
 
-	if err == heartbleed.Safe {
-		res.Vulnerable = false
-		log.Printf("%s (%s:%d): %#v\n", req.Hostname, req.Host, req.Port, res.Vulnerable)
-	} else if err != nil {
-		log.Printf("ERROR: %s (%s:%d): %s\n", req.Hostname, req.Host, req.Port, res.Error)
+	cert, err := getCert(req.Hostname, req.Host, req.Port)
+	if err != nil {
 		res.Error = err.Error()
-	} else {
-		res.Vulnerable = true
-		log.Printf("%s (%s:%d): %#v\n", req.Hostname, req.Host, req.Port, res.Vulnerable)
+	}
+	if cert != nil {
+		res.Cert = *cert
 	}
 
 	bytes, joerr := json.Marshal(res)
 	if joerr != nil {
 		log.Println("JSON response marshal Error: ", jierr.Error())
 	}
+	fmt.Println(string(bytes))
 	nc.Publish(reply, bytes)
-
 }
 
 func main() {
@@ -85,7 +149,7 @@ func main() {
 
 	natsChannel := os.Getenv("NATS_CHANNEL")
 	if natsChannel == "" {
-		natsChannel = "vuln.HEARTBLEED.*"
+		natsChannel = "get.CERTPUMP.*"
 	}
 
 	var nc *nats.Conn
@@ -108,10 +172,10 @@ func main() {
 		log.Fatalln(err.Error())
 	}
 
-	log.Println("Connected to Nats (", natsURL, ")")
+	log.Println("CertPump Connected to Nats (", natsURL, ")")
 
 	// subscribe and spawn a goroutine for each message
-	_, err = nc.QueueSubscribe(natsChannel, "heartbleed_group", func(msg *nats.Msg) {
+	_, err = nc.QueueSubscribe(natsChannel, "certpump_group", func(msg *nats.Msg) {
 		go handleRequest(nc, msg.Data, msg.Reply)
 	})
 
---
+++ CertPump_test.go
@@ -0,0 +1,173 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+	"time"
+
+	nats "github.com/nats-io/go-nats"
+)
+
+func TestSSLping(t *testing.T) {
+	var nc *nats.Conn
+	nc, _ = nats.Connect("nats://localhost:4222")
+
+	req := request{
+		Hostname: "sslping.com",
+		Host:     "195.154.227.44",
+		Port:     443,
+	}
+	bytes, _ := json.Marshal(req)
+	msg, err := nc.Request("get.CERTPUMP.US", bytes, time.Second*100)
+	if err != nil {
+		fmt.Println(err)
+	}
+	res := response{}
+	json.Unmarshal(msg.Data, &res)
+
+	if res.Host != req.Host {
+		t.Fatal("Host changed")
+	}
+	if res.Port != req.Port {
+		t.Fatal("Port changed")
+	}
+	if res.Hostname != req.Hostname {
+		t.Fatal("Hostname changed")
+	}
+	if res.Error != "" {
+		t.Fatal("Error not nil", res.Error)
+	}
+}
+
+func TestPlacenames(t *testing.T) {
+	var nc *nats.Conn
+	nc, _ = nats.Connect("nats://localhost:4222")
+
+	req := request{
+		Hostname: "www.placenames.com",
+		Host:     "104.28.13.117",
+		Port:     443,
+	}
+	bytes, _ := json.Marshal(req)
+	msg, err := nc.Request("get.CERTPUMP.US", bytes, time.Second*100)
+	if err != nil {
+		fmt.Println(err)
+	}
+	res := response{}
+	json.Unmarshal(msg.Data, &res)
+
+	if res.Host != req.Host {
+		t.Fatal("Host changed")
+	}
+	if res.Port != req.Port {
+		t.Fatal("Port changed")
+	}
+	if res.Hostname != req.Hostname {
+		t.Fatal("Hostname changed")
+	}
+	if res.Error != "" {
+		t.Fatal("Error not nil", res.Error)
+	}
+}
+
+func TestFail1(t *testing.T) {
+	var nc *nats.Conn
+	nc, _ = nats.Connect("nats://localhost:4222")
+
+	req := request{
+		Hostname: "ems.med.unsw.edu.au",
+		Host:     "149.171.203.11",
+		Port:     443,
+	}
+	bytes, _ := json.Marshal(req)
+	msg, err := nc.Request("get.CERTPUMP.US", bytes, time.Second*100)
+	if err != nil {
+		fmt.Println(err)
+	}
+	res := response{}
+	json.Unmarshal(msg.Data, &res)
+
+	if res.Host != req.Host {
+		t.Fatal("Host changed")
+	}
+	if res.Port != req.Port {
+		t.Fatal("Port changed")
+	}
+	if res.Hostname != req.Hostname {
+		t.Fatal("Hostname changed")
+	}
+	if len(res.Cert.Chain) != 1 {
+		t.Fatal("Not one cert in chain")
+	}
+	if res.Error != "x509: incomplete cert chain" {
+		t.Fatal("Error not detected", res.Error)
+	}
+}
+func TestFail2(t *testing.T) {
+	var nc *nats.Conn
+	nc, _ = nats.Connect("nats://localhost:4222")
+
+	req := request{
+		Hostname: "mail.farelogix.com",
+		Host:     "72.46.248.210",
+		Port:     995,
+	}
+	bytes, _ := json.Marshal(req)
+	msg, err := nc.Request("get.CERTPUMP.US", bytes, time.Second*100)
+	if err != nil {
+		fmt.Println(err)
+	}
+	res := response{}
+	json.Unmarshal(msg.Data, &res)
+
+	if res.Host != req.Host {
+		t.Fatal("Host changed")
+	}
+	if res.Port != req.Port {
+		t.Fatal("Port changed")
+	}
+	if res.Hostname != req.Hostname {
+		t.Fatal("Hostname changed")
+	}
+	if len(res.Cert.Chain) != 1 {
+		t.Fatal("Not one cert in chain")
+	}
+	if res.Error != "x509: self signed cert" {
+		t.Fatal("Error not detected", res.Error)
+	}
+}
+
+func TestFail3(t *testing.T) {
+	var nc *nats.Conn
+	nc, _ = nats.Connect("nats://localhost:4222")
+
+	req := request{
+		Hostname: "clickholdings.co.uk",
+		Host:     "77.72.203.173",
+		Port:     443,
+	}
+	bytes, _ := json.Marshal(req)
+	msg, err := nc.Request("get.CERTPUMP.US", bytes, time.Second*100)
+	if err != nil {
+		fmt.Println(err)
+	}
+	res := response{}
+	json.Unmarshal(msg.Data, &res)
+
+	if res.Host != req.Host {
+		t.Fatal("Host changed")
+	}
+	if res.Port != req.Port {
+		t.Fatal("Port changed")
+	}
+	if res.Hostname != req.Hostname {
+		t.Fatal("Hostname changed")
+	}
+	if len(res.Cert.Chain) != 1 {
+		t.Fatal("Not one cert in chain")
+	}
+	if res.Error != "x509: self signed cert" {
+		t.Fatal("Error not detected", res.Error)
+	}
+}
